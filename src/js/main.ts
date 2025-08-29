4c70a4bbca25730af4fe6ccde2e0fa3b1c8d09ea
Nurjigit <n.melisuulu@innopolis.ru>
2025-08-29 21:24:07 +0600

    feat: added page technologies

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,9 +1,11 @@
 import "virtual:svg-icons-register";
 import "../scss/style.scss";
 import science from "./science";
+import product from "./product";
 
 document.addEventListener("DOMContentLoaded", () => {
   science();
+  product();
 });
 
 window.addEventListener("load", () => {
