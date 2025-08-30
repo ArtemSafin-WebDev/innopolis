ad3220790bd676fa7f361403784429d5b57d095a
Artem Safin <artemsafin.webdev@gmail.com>
2025-08-31 01:35:17 +0300

    Блоки

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,9 +1,15 @@
 import "virtual:svg-icons-register";
 import "../scss/style.scss";
 import science from "./science";
+import ticker from "./ticker";
+import education from "./education";
+import technologies from "./technologies";
 
 document.addEventListener("DOMContentLoaded", () => {
   science();
+  ticker();
+  education();
+  technologies();
 });
 
 window.addEventListener("load", () => {
