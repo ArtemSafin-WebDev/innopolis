38367248c5f0c95a133ae8123536e2bb12e9c239
Lisarusx1 <artik19012001@gmail.com>
2025-09-04 15:17:36 +0500

    capab

--- pages.config.js
+++ pages.config.js
@@ -1,9 +1,11 @@
 import home from "./pages-data/home";
 import science from "./pages-data/home";
+import magistracy from "./pages-data/magistracy";
 
 const pagesConfig = {
   ...home,
   ...science,
+  ...magistracy,
 };
 
 export default pagesConfig;
